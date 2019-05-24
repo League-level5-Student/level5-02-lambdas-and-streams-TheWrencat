58677aa15827aae61265ded5482895af3164e14b
League Student <league@iMac.attlocal.net>
2019-05-23 19:00:52 -0700

    finished module

--- src/_04_Searching_With_Streams/StreamSearcher.java
+++ src/_04_Searching_With_Streams/StreamSearcher.java
@@ -10,12 +10,17 @@ public class StreamSearcher {
 	// use Streams to help us out.
 	public long containsCount(String[] strArr, String strToCount) {
 		//2. Convert the String array to a stream
-		
+		Stream<String> toSearch = Arrays.stream(strArr);
 		//3. Use the filter method with a lambda to extract all the String
 		//   elements that match the String parameter.
-		
+		toSearch = toSearch.filter((String s)->{
+			if(s.equals(strToCount)) {
+				return true;
+			}else {
+				return false;
+			}
+		});
 		//4. Return the count of the filtered stream.
-		
-		return 0;
+		return toSearch.count();
 	}
 }
